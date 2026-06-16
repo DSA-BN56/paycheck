242653a8608d6b8d09878d27ac2443d521eb4a8f
aboosufiyans <abu.sufiyan@healthsigns.ai>
2026-06-16 13:25:40 +0530

    First fifa corrected commit

--- old/dashboard.js
+++ old/dashboard.js
@@ -2,20 +2,43 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/12.1.0/firebas
 
 import {
   getAuth,
-  signInWithEmailAndPassword
+  signInWithEmailAndPassword,
+  onAuthStateChanged,
+  signOut
 } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-auth.js";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyA3nDecvFnkb0beOT_QR3o5FrS6tWZ3pgs",
-    authDomain: "paycheck-7382c.firebaseapp.com",
-    projectId: "paycheck-7382c",
-    storageBucket: "paycheck-7382c.firebasestorage.app",
-    messagingSenderId: "580764201388",
-    appId: "1:580764201388:web:8e95ec0dd38d37af308a09"
-  };
+  apiKey: "AIzaSyA3nDecvFnkb0beOT_QR3o5FrS6tWZ3pgs",
+  authDomain: "paycheck-7382c.firebaseapp.com",
+  projectId: "paycheck-7382c",
+  storageBucket: "paycheck-7382c.firebasestorage.app",
+  messagingSenderId: "580764201388",
+  appId: "1:580764201388:web:8e95ec0dd38d37af308a09"
+};
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
+//Protect Dashboard
+onAuthStateChanged(auth, (user) => {
+  if (!user) {
+    window.location.href = "index.html";
+    return;
+  }
+  console.log("Logged in as", user.email);
+});
+
+//logout button
+
+document.getElementById("logoutButton").addEventListener("click", async () => {
+  try {
+    await signOut(auth);
+    window.location.href = "index.html";
+  } catch (error) {
+    alert("Error Signing Out");
+    console.error(error);
+  }
+});
+
 
 document
   .getElementById("loginButton")
@@ -41,4 +64,4 @@ document
       alert("Login Failed");
     }
 
-});
+  });
