d59b0a8cc8466b9b5eb1ed5d03a10c81a33d1768
aboosufiyans <abu.sufiyan@healthsigns.ai>
2026-06-26 11:53:12 +0530

    login.js missed curly braces in the import. Corrected

--- login.js
+++ login.js
@@ -7,18 +7,10 @@ import {
   signInWithEmailAndPassword
 } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-auth.js";
 
-import firebaseConfig
+import { firebaseConfig }
   from "./firebasesConfig.js";
 
-const firedbaseConfig = {
-  apiKey: "AIzaSyCL9t51pGmVPTiRsQLSrSe35ZwdglHtXPI",
-  authDomain: "sms-speaker-45f37.firebaseapp.com",
-  databaseURL: "https://sms-speaker-45f37-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "sms-speaker-45f37",
-  storageBucket: "sms-speaker-45f37.firebasestorage.app",
-  messagingSenderId: "501657520398",
-  appId: "1:501657520398:web:98f522df504de369852f5f"
-};
+
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 
